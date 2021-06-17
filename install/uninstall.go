5e6a31b45a1e8d54563a64d39560b512a2a39dbd
Bruno Miguel Custódio <brunomcustodio@gmail.com>
2021-06-17 08:31:15 +0100

    connectivity: use the right test namespace in cnps
    Make sure that we install CPNs into the right namespace, as well as that
    any rules reference this namespace. A placeholder had to be picked (or
    otherwise all rules iterated, which is harder to get right) so I am
    picking an envsubst-compatible one just in case we ever need to pipe
    these into 'kubectl' for testing.
    
    Signed-off-by: Bruno Miguel Custódio <brunomcustodio@gmail.com>

--- install/uninstall.go
+++ install/uninstall.go
@@ -58,7 +58,7 @@ func (k *K8sUninstaller) Uninstall(ctx context.Context) error {
 		return err
 	}
 
-	k.Log("🔥 Deleting cilium-test namespace...")
+	k.Log("🔥 Deleting the %q test namespace...", k.params.TestNamespace)
 	k.client.DeleteNamespace(ctx, k.params.TestNamespace, metav1.DeleteOptions{})
 
 	k.Log("🔥 Deleting Service accounts...")
