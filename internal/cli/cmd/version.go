9510a84abd6c12c3b748383ed74e23db28b37a84
zhaojizhuang <571130360@qq.com>
2021-08-12 20:19:29 +0800

    add support display running version
    Signed-off-by: zhaojizhuang <571130360@qq.com>

--- internal/cli/cmd/version.go
+++ internal/cli/cmd/version.go
@@ -15,8 +15,12 @@
 package cmd
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"github.com/cilium/cilium-cli/internal/k8s"
 	"io"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"net/http"
 	"runtime"
 	"strings"
@@ -31,6 +35,61 @@ var (
 	// Version is the software version.
 	Version string
 )
+
+type CiliumVersion struct {
+	client *k8s.Client
+}
+
+func NewCiliumVersion() *CiliumVersion {
+	return &CiliumVersion{client: k8sClient}
+}
+
+func (c *CiliumVersion) GetRunningCiliumVersion(ctx context.Context) (string, error) {
+	nss, err := c.client.ListNamespaces(ctx, metav1.ListOptions{})
+	if err != nil {
+		return "", fmt.Errorf("unable to list k8s namespace: %w", err)
+	}
+
+	// First look for ns kube-system
+	version, err := c.getCiliumVersionOfNamespace(ctx, "kube-system")
+	if version != "" {
+		return version, nil
+	}
+
+	if len(nss.Items) > 0 {
+		for _, namespace := range nss.Items {
+			version, err := c.getCiliumVersionOfNamespace(ctx, namespace.Name)
+			if version != "" {
+				return version, nil
+			}
+			if err != nil {
+				return "", err
+			}
+		}
+	}
+
+	return "", errors.New("unable to obtain cilium version: no cilium pods found")
+}
+
+func (c *CiliumVersion) getCiliumVersionOfNamespace(ctx context.Context, namespace string) (string, error) {
+	pods, err := c.client.ListPods(ctx, namespace, metav1.ListOptions{LabelSelector: "k8s-app=cilium"})
+	if err != nil {
+		return "", fmt.Errorf("unable to list cilium pods: %w", err)
+	}
+	if len(pods.Items) > 0 && len(pods.Items[0].Spec.Containers) > 0 {
+		image := pods.Items[0].Spec.Containers[0].Image
+		version := strings.SplitN(image, ":", 2)
+		if len(version) != 2 {
+			return "", errors.New("unable to extract cilium version from container image")
+		}
+		v := version[1]
+		if digest := strings.Index(v, "@"); digest > 0 {
+			v = v[:digest]
+		}
+		return v, nil
+	}
+	return "", nil
+}
 
 func getLatestStableVersion() string {
 	resp, err := http.Get("https://raw.githubusercontent.com/cilium/cilium/master/stable.txt")
@@ -48,16 +107,27 @@ func getLatestStableVersion() string {
 }
 
 func newCmdVersion() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Display detailed version information",
 		Long:  `Displays information about the version of this software.`,
-		Run: func(cmd *cobra.Command, _ []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			// TODO: add support for reporting the Cilium version running in
 			// the cluster, if any. See https://github.com/cilium/cilium-cli/issues/131
 			fmt.Printf("cilium-cli: %s compiled with %v on %v/%v\n", Version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
 			fmt.Printf("cilium image (default): %s\n", defaults.Version)
 			fmt.Printf("cilium image (stable): %s\n", getLatestStableVersion())
+			ciliumVersion := NewCiliumVersion()
+			version, err := ciliumVersion.GetRunningCiliumVersion(context.Background())
+			if err != nil {
+				fmt.Printf("cilium image (running): unknown,msg: %s %s\n", err.Error())
+			} else {
+				fmt.Printf("cilium image (running): %s\n", version)
+			}
+			return nil
 		},
 	}
+
+	cmd.Flags().StringVar(&contextName, "context", "", "Kubernetes configuration context")
+	return cmd
 }
